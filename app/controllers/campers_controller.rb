ee82b13b4f50881af80146331737ac44329c44da
LaurenGifford <lauren.gifford.17@gmail.com>
2021-01-15 11:41:46 -0500

    completed deliverables 4 and 5

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -8,4 +8,19 @@ class CampersController < ApplicationController
         @camper = Camper.find(params[:id])
     end
 
+    def new
+        @camper = Camper.new
+    end
+
+    def create
+        camper = Camper.create(camper_params(:name, :age))
+        redirect_to camper_path(camper)
+    end
+
+    private
+
+    def camper_params(*args)
+        params.require(:camper).permit(*args)
+    end
+
 end
--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,33 @@
 class SignupsController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+        @signup = Signup.new
+    end
+
+    def create
+        signup = Signup.create(signup_params(:camper_id, :activity_id, :time))
+        redirect_to camper_path(signup.camper)
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+    def signup_params(*args)
+        params.require(:signup).permit(*args)
+    end
 end
